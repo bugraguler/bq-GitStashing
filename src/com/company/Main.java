d704af5f4cd9090cd3e46ed18fd6856b307a0387
bugraguler <bgrglr.be@gmail.com>
2023-08-03 12:47:43 +0200

    the red audi car is ready with some basic fatures

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-    BasicFeatures basicFeatures= new BasicFeatures();
+        BasicFeatures audi = new BasicFeatures();
+        audi.engineCapacity = "2000";
+        audi.color = "red";
+        audi.doors = "5";
+        audi.headlights = "blue";
+
     }
 }
