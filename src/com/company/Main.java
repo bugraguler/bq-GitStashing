dde51a958e250b93500194a2f0a9d2cb6cac515f
bugraguler <bgrglr.be@gmail.com>
2023-10-31 16:30:43 +0100

    new car addedddd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,13 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         BasicFeatures audi = new BasicFeatures();
-        audi.engineCapacity = "2000";
-        audi.color = "red";
-        audi.doors = "5";
+        audi.engineCapacity = "3500";
+        audi.color = "kirmizi";
+        audi.doors = "2";
         audi.headlights = "mavi";
 
         //lets write own code
-        System.out.println("welcome the radio 88mhz");
-        System.out.println("new car with good music");
+        System.out.println("new car added");
 
     }
 }
